4439193507683a5fe894d4145b78828801079d9a
aakashop1995 <aakashop1995@gmail.com>
2026-05-17 12:27:19 +0530

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Flask is working!"
+    return "Flask OK"
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
