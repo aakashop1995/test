f1de6ca28de43280503d5e3f9bead7bbee8c5d09
aakashop1995 <aakashop1995@gmail.com>
2026-05-17 11:34:22 +0530

    Update simple_stream.py

--- simple_stream.py
+++ simple_stream.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response
 from picamera2 import Picamera2
 import cv2
+import time
 
 app = Flask(__name__)
 
@@ -8,16 +9,19 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_preview_configuration(main={"size": (320, 240)}))
 picam2.start()
 
+time.sleep(2)
+
 def generate_frames():
     while True:
         frame = picam2.capture_array()
 
-        # Convert to JPEG
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+        time.sleep(0.01)
 
 @app.route('/')
 def index():
@@ -29,4 +33,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=False, threaded=True)
